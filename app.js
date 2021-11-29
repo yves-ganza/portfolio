15e514cc89c3b9794866155103197af035fb7f9b
lebonYG <lebonganza@outlook.com>
2021-11-29 12:49:47 -0500

    Added menu toggler button

---
+++ app.js
@@ -0,0 +1,21 @@
+const goUpButton = document.getElementById('goUp')
+const navToggler = document.getElementById('nav-toggler')
+const responsiveNav = document.getElementById('responsive-nav')
+
+
+navToggler.onclick = (e) => {
+    e.stopPropagation()
+    let list = responsiveNav.classList
+    list.contains('-right-full') ? 
+    list.replace('-right-full', 'right-0') : 
+        list.replace('right-0', '-right-full')
+}
+
+document.body.onclick = (e) => {
+    let list = responsiveNav.classList
+    list.contains('right-0') ? list.replace('right-0', '-right-full') : ''
+}
+
+goUpButton.onclick = () => {
+    window.scrollTo(0,0)
+}
